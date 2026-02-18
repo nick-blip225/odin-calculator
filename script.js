5647766cf30a39a817f94a55f255ff6f22b628b4
nick-blip225 <224284647+nick-blip225@users.noreply.github.com>
2026-02-17 22:20:52 -0600

    Create functions for math operations (+,-,*,/)
    Created js functions for addition, subtraction, multiplying and dividing
    two numbers at a time.

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNums(a, b) {
+    return parseInt(a + b);
+}
+
+function subtractNums(a,b) {
+    return a - b;
+}
+
+function multiplyNums(a,b) {
+    return a * b;
+}
+
+function divideNums(a,b) {
+    return Math.floor(a / b);
+}
